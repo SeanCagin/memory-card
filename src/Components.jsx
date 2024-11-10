acdf63f124eaa25c7dbff515680b5f559ec8683e
Sean Uz <cuz3@gatech.edu>
2024-11-09 19:35:31 -0500

    Add gifs

--- src/Components.jsx
+++ src/Components.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import specialRand from "./utility.js";
+import {getRandomElement, shuffle} from "./utility.js";
 import logoSrc from "/logo.svg";
 
 const GAMESTATES = {
@@ -14,7 +14,43 @@ const DIFFICULTIES = {
     HARD: 2,
 }
 
-const CARDS = ["mordecai", "benson", "margaret", "CJ", "rigby", "eileen", "skips", "pops", "musclem man", "highfive ghost", "don", "death"];
+const CHARACTERS = {
+    "Mordecai": "https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExcHppcm41bDF6bzNxdGRzNmp5Mnlzb243Y285ajl3dHFqc3p5dnR3biZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/ivxJxZcowzMdY3aF3A/giphy.webp",
+    "Benson": "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExMDN0OXc0aW9venllYjA2bGJ5eDdzcm14M3hsbXVpcWY1bG42YWc5MCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/OVHR3Eb88KFzi/giphy.webp",
+    "Death Bear": "https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExN3Rzd25lZW9jbXp5N2JuYWFvZm91cmVka25jcTRlaGd2bmowbDNpdCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/dkWwpmNH1WzT2/giphy.webp",
+    "Muscle Man": "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExZXZhbmEybDkwNWZ6a2U4ZmxhNnc1YXI2d2s4MWd2MHg0NnRueDVhYiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/lmeh2XGMCbCdW/giphy.webp",
+    "Rigby": "https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExb3VndTkzdXkxN2p3bWFmcnpmZXVoZ2N4c3ptZTNteTdnd2h0NGtxYyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/XgjP1yCu4y2ZbnDIhR/giphy.webp",
+    "Pops": "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExaDIwNTdyNzY4c3lxNTFrbWkxeTA1bDB3YndzMXk0N3Zpb2FwMzNkbSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/iZ0pJCIN1el8Y/giphy.webp",
+    "Margaret": "https://media1.tenor.com/m/AoOIvlfj6lYAAAAd/tengo-tanto-que-contarte-margarita.gif",
+    "Eileen": "https://media1.tenor.com/m/HLQkXAofwcwAAAAd/there-there-soothe.gif",
+    "Skips": "https://media1.tenor.com/m/_MxYn__0gbEAAAAC/regular-show-skips.gif",
+    "Thomas": "https://media1.tenor.com/m/ejW5QVz8Un8AAAAd/regular-show.gif",
+    "Fives": "https://media1.tenor.com/m/1276GVEXs84AAAAd/high-five-ghost-regular-show.gif",
+    "Don": "https://media1.tenor.com/m/OaMQPLExzrMAAAAd/regularshow-hug.gif",
+    "Death": "https://y.yarn.co/cb08e357-b7e0-439a-ab8c-798f1722bde2_text.gif",
+    "Anti Pops": "https://media1.tenor.com/m/E4LLHy0FCBoAAAAd/anti-pops-evil.gif",
+    "Celia": "https://media1.tenor.com/m/adpKYpSoH5AAAAAd/girar-fantasmin.gif",
+    "Mr. Maellard": "https://media1.tenor.com/m/nfOPbIZ4p3MAAAAd/wipe-tears-mr-maellard.gif",
+    "Starla": "https://media1.tenor.com/m/pmP8DAGjfCUAAAAC/starla-on-the-glass-regular-show.gif",
+    "Gary": "https://media1.tenor.com/m/EePFAMcNrO0AAAAd/regular-show-gary-regular-show.gif",
+    "Guardians of Eternal Youth": "https://media1.tenor.com/m/HEr7kmeYi5wAAAAd/regular-show.gif",
+    "Garett Bobby Furgeson": "https://media1.tenor.com/m/N2ncytySZLQAAAAd/regularshow-gbf.gif",
+}
+
+const EASY = 8;
+const NORMAL = 12;
+const HARD = 16;
+
+const CARDS_IMPOSSIBLE = [];
+
+for (let key in CHARACTERS) {
+    CARDS_IMPOSSIBLE.push(key);
+}
+
+const CARDS_EASY = CARDS_IMPOSSIBLE.slice(0, EASY);
+const CARDS_NORMAL = CARDS_IMPOSSIBLE.slice(0, NORMAL);
+const CARDS_HARD = CARDS_IMPOSSIBLE.slice(0, HARD);
+
 
 function Card({imgSrc, clickFunc, name}) {
     return (
@@ -30,12 +66,12 @@ function Page() {
     return (
         <div className="page">
             <Controls />
-            <Gameboard diffculty={DIFFICULTIES.EASY} numCardsShown={4} />
+            <Gameboard CARDS={CARDS_IMPOSSIBLE} numCardsShown={4} />
         </div>
     );
 }
 
-function Gameboard({diffculty, numCardsShown}) {
+function Gameboard({diffculty, numCardsShown, CARDS}) {
     const [gameState, updateGameState] = useState(GAMESTATES.INTERMISSION);
     const [usedCards, updateUsedCards] = useState([]);
     const unusedCards = CARDS.filter(card => {return usedCards.indexOf(card) == -1});
@@ -55,17 +91,18 @@ function Gameboard({diffculty, numCardsShown}) {
     }
 
     renderCards = [];
-    renderCards.push(specialRand(unusedCards));
+    renderCards.push(getRandomElement(unusedCards));
     while (renderCards.length < numCardsShown) {
-        let randCard = specialRand(CARDS);
+        let randCard = getRandomElement(CARDS);
         if (renderCards.indexOf(randCard) == -1) {
             renderCards.push(randCard);
         }
         // handle API calls
     }
+    shuffle(renderCards);
 
 
-    renderArray = renderCards.map((item) => <Card imgSrc={logoSrc /*API CALL VAL*/} clickFunc={clickFunc} name={item} key={item} />);
+    renderArray = renderCards.map((item) => <Card imgSrc={CHARACTERS[item]} clickFunc={clickFunc} name={item} key={item} />);
     return (<div className="cardList">
         {renderArray}
     </div>);
@@ -77,5 +114,7 @@ function Controls() { // Score and Best score
         Best Score: 5
     </div>
 }
+
+
 
 export default Page;
--- src/utility.js
+++ src/utility.js
@@ -1,5 +1,26 @@
-function getRandomIndex(arr) {
-  return arr[Math.floor(Math.random() * arr.length)];
+function getRandomIndex(upper) {
+  return Math.floor(Math.random() * upper);
 }
 
-export default getRandomIndex;
+function getRandomElement(arr) {
+  return arr[getRandomIndex(arr.length)];
+}
+
+function shuffle(array) {
+  let currentIndex = array.length;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+    // Pick a remaining element...
+    let randomIndex = getRandomIndex(currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+}
+
+export { getRandomElement, shuffle };
